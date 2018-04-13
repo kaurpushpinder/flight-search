5fd5a980de5421925c658413e49e9967ab7fa723
Pushpinder Kaur <pushpinder.k@hcl.com>
2018-04-13 16:59:56 +1000

    small fixes

--- src/app/search-form/search-result-refiner/search-result-refiner.component.ts
+++ src/app/search-form/search-result-refiner/search-result-refiner.component.ts
@@ -9,27 +9,27 @@ import { Component, OnInit } from '@angular/core';
 export class SearchResultRefinerComponent implements OnInit {
 
   constructor(private flightService: FlightSearchService) { }
-  priceRangeConfig: any = {
-    behaviour: 'drag',
-    connect: true,
-    margin: 1,
-    range: {
-      min: 0,
-      max: 15000
-    },
-    pips: {
-      mode: 'steps',
-      density: 5,
-      step: 10,
-      stepped: true
-    }
-  };
-  priceRange: Number[] = [100, 6000];
+  priceRangeConfig: any;
+  priceRange: Number[];
   ngOnInit() {
     const range: Number[] = this.flightService.getPriceRange();
     this.priceRange = [range[0], range[1]];
+    this.priceRangeConfig = {
+      behaviour: 'drag',
+      connect: true,
+      margin: 1,
+      range: {
+        min: 0,
+        max: this.priceRange[1].valueOf() + 1000
+      },
+      pips: {
+        mode: 'steps',
+        density: 5,
+        step: 10,
+        stepped: true
+      }
+    };
   }
-  
   rangeChanged() {
     this.flightService.filterResults(this.priceRange);
   }
